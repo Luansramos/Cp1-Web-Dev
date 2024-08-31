a754fc430d47fd5ac1c05127328ffc259e096d3b
luansramos <luanramosgarciadesouza014@gmail.com>
2024-08-30 21:16:37 -0300

    estrutura da pagina sobre criada

--- cp1-web-dev-2s/src/App.jsx
+++ cp1-web-dev-2s/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Header from "./componentes/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Sobre from "./pages/Sobre";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Sobre" element={<Sobre />} />
         </Routes>
       </div>
     </Router>
---
+++ cp1-web-dev-2s/src/pages/Sobre.jsx
@@ -0,0 +1,39 @@
+import "./Sobre.css"
+
+function Sobre(){
+    return(
+
+        <body>
+            <div className="tudo">
+                <section className="container-1">
+
+                    <div className="texto">
+                        <h1 className="titulo">Bem- vindo à revolução dos vídeos</h1>
+
+                        <p className="descricao">Somos uma startup com o objetivo de facilitar a produção de conteúdo em larga escala com qualidade profissional, capaz de atingir uma audiência de milhões de seguidores.</p>
+
+                    </div>
+                    <button>Baixe o app</button>
+
+                </section>
+
+                <figure className="container-2">
+                    
+                    <div className="imagem-1">
+                        a
+                    </div>
+
+
+                    <div className="imagem-2">
+                        b
+                    </div>  
+                </figure>
+
+
+
+            </div>
+        </body>
+
+    )
+}
+export default Sobre;
